988d236c934ed43cc2e5fa6271b4b5329d0cfbac
arunkumar-r-r <rrarunkumardgct@gmail.com>
2022-07-22 08:19:08 +0530

    added thread line element to comment container

--- src/components/CommentBoxContainer/CommentBoxContainer.js
+++ src/components/CommentBoxContainer/CommentBoxContainer.js
@@ -1,27 +1,34 @@
 import Button from "../Button/Button";
 import CommentBox from "../CommentBox/CommentBox";
 import "./CommentBoxContainer.scss";
+
 const CommentBoxContainer = (props) => {
   const handleClick = () => {
     console.log("clicked");
   };
+
   return (
-    <div className="commentBoxContainer">
-      <div className="commentbox-body">
-        <CommentBox placeholder={"What are your thoughts?"} />
+    <div className="wrapper">
+      <div className="divider">
+        <div className="threadline"></div>
       </div>
-      <div className="commentbox-footer">
-        <Button
-          classname={"right-spacing-12"}
-          type="gost"
-          onClick={() => handleClick()}
-        >
-          cancel
-        </Button>
-        <Button type="primary" onClick={() => handleClick()}>
-          Reply
-        </Button>
-      </div>{" "}
+      <div className="commentBoxContainer">
+        <div className="commentbox-body">
+          <CommentBox placeholder={"What are your thoughts?"} />
+        </div>
+        <div className="commentbox-footer">
+          <Button
+            classname={"right-spacing-12"}
+            type="gost"
+            onClick={() => handleClick()}
+          >
+            cancel
+          </Button>
+          <Button type="primary" onClick={() => handleClick()}>
+            Reply
+          </Button>
+        </div>{" "}
+      </div>
     </div>
   );
 };
