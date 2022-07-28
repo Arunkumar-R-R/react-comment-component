60f6c5c7ec5764affd3eade87c5de616a6fbb17c
arunkumar-r-r <rrarunkumardgct@gmail.com>
2022-07-28 11:55:54 +0530

    updated Mock API data

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,26 +2,26 @@ export const getComments = async () => {
   return [
     {
       id: "1",
-      body: "it is one of the greatest library in js ecosystem",
+      text: "it is one of the greatest library in js ecosystem",
       username: "Arun",
       userId: "1",
     },
     {
       id: "2",
-      body: "It is easiest one compared to angular",
-      username: "Karishma",
+      text: "It is easiest one compared to angular",
+      username: "Sandra",
       userId: "2",
     },
     {
       id: "3",
-      body: "Hooks are one of the best feature of react",
-      username: "Dhanakoti",
+      text: "Hooks are one of the best feature of react",
+      username: "Willsmith",
       userId: "3",
     },
     {
       id: "4",
-      body: "Second comment second child",
-      username: "Dharshini",
+      text: "Second comment second child",
+      username: "Denzal washington",
       userId: "3",
     },
   ];
