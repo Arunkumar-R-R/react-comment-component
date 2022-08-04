81de40e36bb54771bdb1b7d41a7b5babb4a5e931
arunkumar-r-r <rrarunkumardgct@gmail.com>
2022-08-04 22:17:54 +0530

    Added onClose functionality for modal component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,14 @@
 import { createPortal } from "react-dom";
 import "./Modal.scss";
 const Modal = (prop) => {
-  const { children } = prop;
+  const { children, onClose } = prop;
   const modalRoot = document.getElementById("modal-root");
 
   return createPortal(
-    <div className="modal">
-      <div className="modal-body">{children}</div>
+    <div className="modal" onClick={onClose}>
+      <div className="modal-body" onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
     </div>,
     modalRoot
   );
