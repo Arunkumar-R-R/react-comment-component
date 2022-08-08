353dcf46347c4abff9587edb40ecbb162ce89df6
arunkumar-r-r <rrarunkumardgct@gmail.com>
2022-08-08 11:21:55 +0530

    Update: overflow hidden for body when modal opened

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import "./Modal.scss";
 const Modal = (prop) => {
   const { children, onClose } = prop;
   const modalRoot = document.getElementById("modal-root");
+
+  useEffect(() => {
+    let body = document.querySelector("body");
+    body.style.overflow = "hidden";
+
+    return () => {
+      body.style.overflow = "auto";
+    };
+  }, []);
 
   return createPortal(
     <div className="modal" onClick={onClose}>
