8c160583619271323092d195672e906267c37384
arunkumar-r-r <rrarunkumardgct@gmail.com>
2022-08-14 22:00:21 +0530

    Update: Removed id's from mock data

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,6 @@
 export const getComments = async () => {
   return [
     {
-      id: "1",
       text: "it is one of the greatest library in js ecosystem",
       username: "Arun",
       userId: "arun10",
@@ -25,7 +24,6 @@ export const getComments = async () => {
       upVoteCount: 0,
     },
     {
-      id: "2",
       text: "It is easiest one compared to angular",
       username: "Sandra",
       userId: "Sandra11",
@@ -33,7 +31,6 @@ export const getComments = async () => {
       upVoteCount: 0,
     },
     {
-      id: "3",
       text: "Hooks are one of the best feature of react",
       username: "Willsmith",
       userId: "willsmith12",
@@ -41,7 +38,6 @@ export const getComments = async () => {
       upVoteCount: 0,
     },
     {
-      id: "4",
       text: "React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies. React can be used as a base in the development of single-page, mobile, or server - rendered applications with frameworks like Next.js. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing, as well as certain client - side functionality.",
       username: "Denzal washington",
       userId: "denzalwashington13",
